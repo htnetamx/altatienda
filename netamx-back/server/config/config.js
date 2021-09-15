8c8c3be524f74e885a0b9400b397c8c6c8dd4130
APS96 <32958004+APS96@users.noreply.github.com>
2021-09-15 17:26:44 -0500

    Merge_Complete

--- netamx-back/server/config/config.js
+++ netamx-back/server/config/config.js
@@ -7,5 +7,5 @@ process.env.SEED = process.env.SEED || 'TjN0QE14MjAyMQ==';
 //database
 process.env.DATABASE_URL = process.env.DATABASE_URL || '127.0.0.1';
 process.env.USER_DATABASE = process.env.USER_DATABASE || 'root';
-process.env.PASSWORD_DATABASE = process.env.PASSWORD_DATABASE || 'Netamx@1324';
+process.env.PASSWORD_DATABASE = process.env.PASSWORD_DATABASE || 'Andresposada015!';
 process.env.NAME_DATABASE = process.env.NAME_DATABASE || 'netamx';
