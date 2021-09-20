3e266d00c0dbb2828f08946a7950f3984fb85e55
APS96 <32958004+APS96@users.noreply.github.com>
2021-09-20 18:10:48 -0500

    Changes_Formatted_Address

--- netamx-back/server/store/models/Store.js
+++ netamx-back/server/store/models/Store.js
@@ -84,7 +84,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     FormattedAddress: {
-      type: DataTypes.STRING(100),
+      type: DataTypes.STRING(1000),
       allowNull: false
     }
   }, {
